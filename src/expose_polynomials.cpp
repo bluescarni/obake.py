d3057829d9e844cb70f8785c10a0bb02ad1e85bc
Francesco Biscani <bluescarni@gmail.com>
2019-10-26 20:45:24 +0200

    Licensing bits.

--- src/expose_polynomials.cpp
+++ src/expose_polynomials.cpp
@@ -1,3 +1,11 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the obake.py library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <mp++/extra/pybind11.hpp>
 
 #include <pybind11/pybind11.h>
--- src/expose_polynomials_real128.cpp
+++ src/expose_polynomials_real128.cpp
@@ -1,3 +1,11 @@
+// Copyright 2019 Francesco Biscani (bluescarni@gmail.com)
+//
+// This file is part of the obake.py library.
+//
+// This Source Code Form is subject to the terms of the Mozilla
+// Public License v. 2.0. If a copy of the MPL was not distributed
+// with this file, You can obtain one at http://mozilla.org/MPL/2.0/.
+
 #include <boost/hana/for_each.hpp>
 
 #include <mp++/config.hpp>
