b1e3a28daa5c85bcb014b53bdff017267c896fba
Francesco Biscani <bluescarni@gmail.com>
2019-10-31 22:36:27 +0100

    Some work on the build system and try adding a py27 build.

---
+++ tools/circleci_bionic_py27_conda.sh
@@ -0,0 +1,37 @@
+#!/usr/bin/env bash
+
+# Echo each command
+set -x
+
+# Exit on error.
+set -e
+
+# Core deps.
+sudo apt-get install build-essential
+
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh;
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+bash miniconda.sh -b -p $HOME/miniconda
+conda config --add channels conda-forge --force
+
+conda_pkgs="cmake>=3.3 obake-devel boost-cpp python=2.7 pybind11"
+
+conda create -q -p $deps_dir -y $conda_pkgs
+source activate $deps_dir
+
+export deps_dir=$HOME/local
+export PATH="$HOME/miniconda/bin:$PATH"
+export PATH="$deps_dir/bin:$PATH"
+
+# Create the build dir and cd into it.
+mkdir build
+cd build
+
+cmake ../ -DCMAKE_INSTALL_PREFIX=$deps_dir -DCMAKE_PREFIX_PATH=$deps_dir -DCMAKE_BUILD_TYPE=Release
+make -j2 VERBOSE=1
+
+python -c "import obake; obake.test.run_test_suite()"
+
+set +e
+set +x
